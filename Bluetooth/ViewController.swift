e441349585a75d9e139fd0b1b09f2d9de31c3605
qinyshen <qinyshen@umich.edu>
2016-11-14 13:45:26 -0500

    first commit

--- Bluetooth/ViewController.swift
+++ Bluetooth/ViewController.swift
@@ -7,18 +7,82 @@
 //
 
 import UIKit
+import Foundation
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var connection: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        //self.message.text = "AA"
+
+        
+//        // Watch Bluetooth connection
+//        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.connectionChanged(_:)), name: NSNotification.Name(rawValue: BLEServiceChangedStatusNotification), object: nil)
+//        
+//        
+//        // Watch message
+//        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.messageChanged(_:)), name: NSNotification.Name(rawValue: MessageNotification), object: nil)
+        
+        // Start the Bluetooth discovery process
+        _ = btDiscoverySharedInstance
+        
+    }
+    
+    //@IBOutlet weak var connection: UITextField!
+    
+//    deinit {
+//        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: BLEServiceChangedStatusNotification), object: nil)
+//    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    
+//    func connectionChanged(_ notification: Notification) {
+//        // Connection status changed. Indicate on GUI.
+//        let userInfo = (notification as NSNotification).userInfo as! [String: Bool]
+//        
+//        DispatchQueue.main.async(execute: {
+//            print("set isConnected text")
+//            
+//            self.connection.text = "connected"
+//            
+//            // Set image based on connection status
+//            if let isConnected: Bool = userInfo["isConnected"] {
+//                if isConnected {
+//                    self.connection.text = "connected"
+//                } else {
+//                    self.connection.text = "Not connected"
+//                }
+//            }
+//        });
+//    }
+//    
+//    
+//    
+//    func messageChanged(_ notification: Notification) {
+//        // Connection status changed. Indicate on GUI.
+//        let userInfo = (notification as NSNotification).userInfo as! [String: String]
+//        
+//        DispatchQueue.main.async(execute: {
+//            print("set message text")
+//
+//            // Set image based on connection status
+//            if let message: String = userInfo["message"] {
+//                self.message.text = message
+//            }
+//        });
+//    }
+//    
+    
 
 
 }
